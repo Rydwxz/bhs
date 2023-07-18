cf16e5b9651cca0254b1843ec42a73d70fc70aa1
rw <collardmusic@gmail.com>
2023-07-18 00:47:32 -0700

    u

---
+++ colors/colors/black_hole_sun.vim
@@ -0,0 +1,4 @@
+lua << EOF
+local black_hole_sun = require("black_hole_sun")
+black_hole_sun.setup({})
+EOF
---
+++ mv.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+mv black_hole_sun/lua lua
+mv black_hole_sun/colors colors
+rm -rf black_hole_sun

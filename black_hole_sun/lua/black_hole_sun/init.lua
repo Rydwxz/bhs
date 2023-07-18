8388de29b3c9b067cb8789c1f4745b72a9dc72c3
rw <collardmusic@gmail.com>
2023-07-18 00:16:49 -0700

    t

---
+++ black_hole_sun/lua/black_hole_sun/init.lua
@@ -0,0 +1,18 @@
+local M = {}
+local theme = require('black_hole_sun.theme')
+
+M.setup = function()
+  vim.cmd('hi clear')
+
+  vim.o.background = 'dark'
+  if vim.fn.exists('syntax_on') then
+    vim.cmd('syntax reset')
+  end
+
+  vim.o.termguicolors = true
+  vim.g.colors_name = 'black_hole_sun'
+
+  theme.set_highlights()
+end
+
+return M

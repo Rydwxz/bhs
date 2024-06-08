bf9a43f647345de90aef4b9b35aa2714a2f39cb3
Redward <collardmusic@gmail.com>
2024-06-08 15:55:46 -0700

    more oranger

--- lua/black_hole_sun/theme.lua
+++ lua/black_hole_sun/theme.lua
@@ -262,9 +262,9 @@ theme.set_highlights = function()
   hl(0, 'LualineCA', { fg = c.neongreen, bg = c.dark })
   hl(0, "LualineCB", { fg = c.star_green, bg = c.dark })
   hl(0, "LualineCC", { fg = c.fg, bg = c.dark })
-  hl(0, 'LualineEA', { fg = c.lightdark, bg = c.dark })
-  hl(0, "LualineEB", { fg = c.lightdark, bg = c.dark })
-  hl(0, "LualineEC", { fg = c.lightdark, bg = c.dark })
+  hl(0, 'LualineEA', { fg = c.solarorange, bg = c.dark })
+  hl(0, "LualineEB", { fg = c.solarorange, bg = c.dark })
+  hl(0, "LualineEC", { fg = c.solarorange, bg = c.dark })
 
   -- Telescope
   hl(0, "TelescopeSelection", { fg = c.hint_blue, bg = 'NONE' })
